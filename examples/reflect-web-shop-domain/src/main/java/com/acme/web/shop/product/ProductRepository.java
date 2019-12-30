2e105cb432e651ee5c2d0b9c0ec9c10b0040711b
nilsth <nilsth@OZLT401.meyn.nl>
2019-12-30 14:18:35 +0100

    Fixed #300

--- examples/reflect-web-shop-domain/src/main/java/com/acme/web/shop/product/ProductRepository.java
+++ examples/reflect-web-shop-domain/src/main/java/com/acme/web/shop/product/ProductRepository.java
@@ -8,11 +8,11 @@ public class ProductRepository {
 	private final List<Product> products;
 
 	public ProductRepository(ProductGenerator productGenerator) {
-		products = productGenerator.generateList(10);
+		products = productGenerator.generateList(20);
 	}
 
 	public List<Product> findProduct(ProductSearchCritiria searchCritiria) {
-		String searchString = (searchCritiria.getSearch()==null)?"":searchCritiria.getSearch().toLowerCase();
+		String searchString = (searchCritiria.getSearch() == null) ? "" : searchCritiria.getSearch().toLowerCase();
 		return products.stream()
 				.filter(p -> (p.getName() != null && p.getName().toLowerCase().contains(searchString))
 						|| (p.getCode() != null && p.getCode().toLowerCase().contains(searchString))
@@ -20,7 +20,7 @@ public class ProductRepository {
 				.collect(Collectors.toList());
 	}
 
-	public List<Product> bestSellingProducts() {
+	public List<Product> allProducts() {
 		return products;
 	}
 
--- examples/reflect-web-shop-domain/src/main/java/com/acme/web/shop/product/ProductService.java
+++ examples/reflect-web-shop-domain/src/main/java/com/acme/web/shop/product/ProductService.java
@@ -6,23 +6,38 @@ import nth.reflect.fw.layer5provider.reflection.behavior.hidden.Hidden;
 import nth.reflect.fw.layer5provider.reflection.behavior.parameterfactory.ParameterFactory;
 
 public class ProductService {
-	 
- 	private final ProductRepository productRepository;
- 
- 	public ProductService(ProductRepository productRepository) {
- 		this.productRepository = productRepository;
- 	}
- 
- 	@ParameterFactory
- 	public List<Product> findProduct(ProductSearchCritiria searchCritiria) {
- 		return productRepository.findProduct(searchCritiria);
- 	}
 
- 	@Hidden(exceptForRoleNames="salesmanager")
-	public List<Product> bestSellingProducts() {
- 		List<Product> products = productRepository.bestSellingProducts();
- 		return products;
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
 	}
- 
- 	// other action methods...
- }
+
+	@ParameterFactory
+	public List<Product> findProduct(ProductSearchCritiria searchCritiria) {
+		return productRepository.findProduct(searchCritiria);
+	}
+
+	public List<Product> specialOffers() {
+		List<Product> products = productRepository.allProducts();
+		return products;
+	}
+
+	public List<Product> recommandedProducts() {
+		List<Product> products = productRepository.allProducts();// TODO
+		return products;
+	}
+
+	@Hidden(exceptForRoleNames = "salesmanager")
+	public List<Product> bestSellingProducts() {
+		List<Product> products = productRepository.allProducts();// TODO
+		return products;
+	}
+
+	public List<Product> allProducts() {
+		List<Product> products = productRepository.allProducts();
+		return products;
+	}
+
+	// other action methods...
+}
