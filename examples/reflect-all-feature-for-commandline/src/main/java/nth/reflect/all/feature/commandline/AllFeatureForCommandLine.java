6295325711c35189eb7a9e52e607f573a1528da9
ntenhoeve <nilstenhoeve@gmail.com>
2020-02-21 19:54:08 +0100

    Fixed #58, Fixed #132

--- examples/reflect-all-feature-for-commandline/src/main/java/nth/reflect/all/feature/commandline/AllFeatureForCommandLine.java
+++ examples/reflect-all-feature-for-commandline/src/main/java/nth/reflect/all/feature/commandline/AllFeatureForCommandLine.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import nth.reflect.all.feature.commandline.dom.AllFeatureServiceObject;
-import nth.reflect.fw.layer5provider.url.UrlProvider;
-import nth.reflect.fw.layer5provider.url.application.ApplicationUrlProvider;
-import nth.reflect.fw.layer5provider.url.classresource.ClassResourceUrlProvider;
 import nth.reflect.fw.ui.commandline.ReflectApplicationForCommandLine;
 
 public class AllFeatureForCommandLine extends
@@ -26,12 +23,5 @@ public class AllFeatureForCommandLine extends
 	public List<Class<?>> getInfrastructureClasses() {
 		return new ArrayList<Class<?>>();
 	}
-
-	
-	@Override
-	public List<Class<? extends UrlProvider>> getUrlProviderClasses() {
-		return Arrays.asList(ClassResourceUrlProvider.class, ApplicationUrlProvider.class);
-	}
-
 
 }
